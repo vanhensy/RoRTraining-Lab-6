f96668e919c64eeb5c4932230704db656f851687
s3515297 <s3515297@rmit.edu.vn>
2016-04-14 19:58:41 +0700

    Milestone 2

--- app/bot/language_bot.rb
+++ app/bot/language_bot.rb
@@ -1,8 +1,20 @@
 class LanguageBot < SlackRubyBot::Bot
- command 'say' do |client, data, match|
+   command 'say' do |client, data, match|
      send_message client, data.channel, match['expression']
    end
+   
    command 'fuck' do |client, data, match|
      send_message client, data.channel, match['expression']
    end
+
+   command 'hello' do |client, data, match|
+      client.say(text: "hello <@#{data.user}>!", channel: data.channel)
+   end
+
+   command 'hello' do |client, data, match|
+       I18n.locale = :vi
+       text = I18n.t "hello", user: user.username
+       client.say(text: text, channel: data.channel)
+   end
+
 end
